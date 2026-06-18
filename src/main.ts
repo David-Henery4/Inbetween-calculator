b9c3dd9e715ccca80f7ba7ddc188c8c4b6905a24
David Henery <david4henery00@hotmail.com>
2026-06-18 13:42:15 +0100

    updated-calc-to-add-to-lowest-input

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,8 @@ formCalculator.addEventListener("submit", (e) => {
   const form = e.currentTarget as HTMLFormElement;
   const values = Object.fromEntries(new FormData(form).entries());
   if (!values["number-1"] && !values["number-2"]) return;
-  resultSpanElement.innerText = `${Math.abs(+values["number-1"] - +values["number-2"]) / 2}`;
+  const inputValue1 = +values["number-1"];
+  const inputValue2 = +values["number-2"];
+  const lowestValueInputed = Math.min(inputValue1, inputValue2);
+  resultSpanElement.innerText = `${Math.abs(inputValue1 - inputValue2) / 2 + lowestValueInputed}`;
 });
